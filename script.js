6bb5fb4f426590ce894f8eed2ee4d1905a49078a
camposfabio987 <campos.fabio987@gmail.com>
2022-05-10 11:38:15 -0300

    cenário finalizado mas sem animação

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
-var x = 200;
-var y = 100;
-var auxX = 50;
-var auxY = 25;
+
 
   // CRIANDO OBJETOS ESTÁTICOS
   ctx.fillStyle = 'rgba(0,255,0,0.5)'; // COR DO CHÃO
-  ctx.fillRect(0, 600,(window.innerWidth), 50); // CHÃO
+  ctx.fillRect(0, 600, (window.innerWidth), 50); // CHÃO
+
+  ctx.fillStyle = 'yellow'; // COR DO PRÉDIO 1
+  ctx.fillRect(0, 300, 250, 20); //PRÉDIO 1 
+
+  ctx.fillStyle = 'red'; // COR DO PRÉDIO 2
+  ctx.fillRect(271, 335, 175, 25); //PRÉDIO 2
   
-  ctx.fillStyle = 'blue'; // COR DO PERSONAGEM PRINCIPAL
-  ctx.fillRect(0, 500, 100, 100); //PERSONAGEM PRINCIPAL
+  ctx.fillStyle = 'orange'; // COR DO PRÉDIO 3, ONDE O PERSONAGEM ENTRA
+  ctx.fillRect(492, 250, 225, 35); //PRÉDIO 3, ONDE O PERSONAGEM ENTRA 
   
+  ctx.fillStyle = 'purple'; // COR DA LOJA 
+  ctx.fillRect(753, 400, 325, 45); //LOJA 
+
+  ctx.fillStyle = 'pink'; // COR DO PRÉDIO 4, Ñ TERÁ INTERAÇÃO
+  ctx.fillRect(1100, 100, 300, 10); // PRÉDIO 4, Ñ HAVERÁ INTERAÇÃO
+
+  // PERSONAGEM PRINCIPAL 
+
+  function TestePP() {
+    requestAnimationFrame(TestePP)
+
+    ctx.fillStyle = 'blue'; // COR DO PERSONAGEM PRINCIPAL
+    ctx.fillRect(0, 225, 75, 75); //PERSONAGEM PRINCIPAL
+  }
+
+ TestePP()
